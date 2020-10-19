a8b636615af099b66dd4b859fb03d9510ddcea5b
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-10-19 19:26:40 +0200

    JScodes

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -3,17 +3,36 @@
 var items = JSON.parse(localStorage.getItem('items')) || [];
 var input = document.querySelector('.input');
 var container = document.querySelector('.container');
+var editable = -1;
 
 var print = function print() {
   var containerContent = '';
-  items.map(function (item) {
-    containerContent = containerContent + "<div class=\"item\">\n            <div class=\"text\">".concat(item, "</div>\n            <div class=\"row\">\n                <button class=\"editButton\">Edit</button>\n                <div class=\"divider\"></div>\n                <button class=\"removeButton\">Remove</button>\n            </div>\n        </div>");
+  items.map(function (item, index) {
+    containerContent = containerContent + "<div class=\"item\">\n            <div class=\"text\" contenteditable=\"".concat(editable === index, "\" >").concat(item, "</div>\n            <div class=\"row\">\n                <button class=\"editButton\" onclick=\"edit(").concat(index, ")\" style=\"display: ").concat(editable === index && editable >= 0 ? "none" : "initial", "\">Edit</button>\n                <button class=\"saveButton\" onclick=\"save(").concat(index, ", this)\" style=\"display: ").concat(editable === index && editable >= 0 ? "initial" : "none", "\">Save</button>\n                <div class=\"divider\"></div>\n                <button class=\"removeButton\" onclick=\"remove(").concat(index, ")\">Remove</button>\n            </div>\n        </div>");
   });
   container.innerHTML = containerContent;
 };
 
 var add = function add() {
   items.push(input.value);
+  localStorage.setItem('items', JSON.stringify(items));
+  print();
+};
+
+var edit = function edit(index) {
+  editable = index;
+  print();
+};
+
+var save = function save(index, that) {
+  items[index] = that.parentElement.parentElement.querySelector(".text").innerHTML;
+  editable = -1;
+  localStorage.setItem('items', JSON.stringify(items));
+  print();
+};
+
+var remove = function remove(index) {
+  items.splice(index, 1);
   localStorage.setItem('items', JSON.stringify(items));
   print();
 };
--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 let items = JSON.parse(localStorage.getItem('items')) || []
 let input = document.querySelector('.input');
 let container = document.querySelector('.container')
+let editable = -1
 
 const print = () => {
     let containerContent = ''
 
-    items.map((item) => {
+    items.map((item, index) => {
         containerContent = containerContent + `<div class="item">
-            <div class="text">${item}</div>
+            <div class="text" contenteditable="${editable===index}" >${item}</div>
             <div class="row">
-                <button class="editButton">Edit</button>
+                <button class="editButton" onclick="edit(${index})" style="display: ${editable===index && editable>=0 ? "none" : "initial"}">Edit</button>
+                <button class="saveButton" onclick="save(${index}, this)" style="display: ${editable===index && editable>=0 ? "initial" : "none"}">Save</button>
                 <div class="divider"></div>
-                <button class="removeButton">Remove</button>
+                <button class="removeButton" onclick="remove(${index})">Remove</button>
             </div>
         </div>`
     })
@@ -26,7 +28,25 @@ const add = () => {
     items.push(input.value);
     localStorage.setItem('items', JSON.stringify(items));
     print();
-
 };
+
+const edit = (index) => {
+    editable = index;
+    print();
+};
+
+const save = (index, that) => {
+    items[index] = that.parentElement.parentElement.querySelector(".text").innerHTML
+    editable = -1;
+    localStorage.setItem('items', JSON.stringify(items));
+    print();
+};
+
+const remove = (index) => {
+    items.splice(index, 1)
+    localStorage.setItem('items', JSON.stringify(items));
+    print();
+};
+
 
 print()
